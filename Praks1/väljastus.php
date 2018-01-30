5bfcb1389034ab262969db140efea33f3b637423
Tsaregorodtsev <v.tsaregorodtsev@khk.ee>
2018-01-30 11:28:19 +0200

    constant defineeritud

--- Praks1/väljastus.php
+++ Praks1/väljastus.php
@@ -20,3 +20,19 @@ $bussiNumber = 4; //täisarv,integer
 $soidukestvus =0.8; //sõidu kestvus, float
 // konstandite defineerimine
 define('number_PI', 3.14);
+//Kasutades eel defineeritud muutujad ja konstandid
+echo 'olen '.$eesnimi.' '.$perenimi.' '.'<br />';
+echo 'soidan koolist kesklinna bussiga number' .$bussiNumber .'<br />';
+echo 'jouan kesklinnas'.$soidukestvus.'tunniga'.'<br />';
+echo 'midagi konstantne PI'.number_PI.'<br />';
+//tingimuslaused
+$ilmväljas ='sajab lund';
+if($ilmväljas =='sajab lund'){
+    echo 'tuleb minna saunas<br />';
+
+} else {
+        echo 'Pole mõtet sauna minna<br />';
+    }
+
+// ? operator  = ternany operator
+echo ($ilmväljas == 'sajab lund') ? tuleb minna saunas
