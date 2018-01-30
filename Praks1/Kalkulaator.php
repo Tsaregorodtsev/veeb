6b04248f92d74f85a64f8c65198d71c3e46c4ed2
Tsaregorodtsev <v.tsaregorodtsev@khk.ee>
2018-01-30 11:31:05 +0200

    väärtused

---
+++ Praks1/Kalkulaator.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: v.tsaregorodtsev
+ * Date: 30.01.2018
+ * Time: 11:28
+ */
+$söögihind = 2.55; //ahjuliha
+$soodustusProtsent = 0.15; //soodus %
+$opilaseToetus = 1.80 // euro
