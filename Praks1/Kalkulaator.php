34554c9ada213202b6f122ec609674881147e85c
Tsaregorodtsev <v.tsaregorodtsev@khk.ee>
2018-01-30 11:41:23 +0200

    kalkulaator

--- Praks1/Kalkulaator.php
+++ Praks1/Kalkulaator.php
@@ -5,6 +5,12 @@
  * Date: 30.01.2018
  * Time: 11:28
  */
-$söögihind = 2.55; //ahjuliha
+$söögiHind = 2.55; //ahjuliha
 $soodustusProtsent = 0.15; //soodus %
-$opilaseToetus = 1.80 // euro
+$opilaseToetus = 1.80;// euro
+//arvutused
+if($söögiHind > 0 and $soodustusProtsent > 0 and $opilaseToetus > 0); {
+    $soodushind = $söögiHind * ((100 - $soodustusProtsent) /100);
+    $hindopilasele = $söögiHind =$opilaseToetus;
+    echo 'soogi hind opilasele = '.$hindopilasele.'<br />';
+}
