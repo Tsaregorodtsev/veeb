ef3cbf6932c301755cf3e9248043f98d46d6f7b4
Tsaregorodtsev <v.tsaregorodtsev@khk.ee>
2018-02-14 11:58:12 +0200

    kalkulaator

---
+++ testfail.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: v.tsaregorodtsev
+ * Date: 14.02.2018
+ * Time: 11:57
+ */
+function Hind($taishind, $sooduskart, $kasOledOpilane)
+{
+    $soodustusProts = 15; // %
+    $soodustus = $taishind;
+    $opilaseToetus = 1.80; // eur, float
+    if ($sooduskart) {
+        $soodustus = $taishind * ((100 - $soodustusProts) / 100);
+    }
+    if($kasOledOpilane){
+        $soodustus = $soodustus - $opilaseToetus;
+    }
+    return $soodustus;
+}
+//func for forms
+//call html from vorm.html ja show
+function loeVormFailist($failnimi){
+    //kontrollime kas fail olemas
+    if (file_exists($failnimi) and is_file($failnimi) and is_readable($failnimi)){
+        //loeb fail
+        $fp = fopen($failnimi, 'r');
+        $sisu = fread($fp, filesize($failnimi));//loeme faili sisu
+        $fp = fclose($fp); //paneme ühendus failiga kinni
+    } else {
+        printf('<br>Probleem on %s', $failnimi);
+    };
+    print ($sisu);
+};
+?>
