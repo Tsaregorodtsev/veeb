824935e286692fcbffadb50ddb9eb10f25f8f7c6
Tsaregorodtsev <v.tsaregorodtsev@khk.ee>
2018-02-21 09:09:16 +0200

    kalkulaator

--- Praks2/mang1.php
+++ Praks2/mang1.php
@@ -5,17 +5,24 @@
  * Date: 14.02.2018
  * Time: 13:24
  */
+// määrame katsete arvu muutujat
+$katseteArv = $_POST['katseteArv'];
+// kontrollime antud väärtuse olemasolu
+$katseteArv = isset($katseteArv) ? ++$katseteArv : 0;
+$serveriArv = isset($serveriArv) ? $serveriArv : rand(1,50);
+// trükime andmetega täidetud vorm
 echo '
     <form method="post" action="'.$_SERVER['PHP_SELF'].'">
         Sisesta arv vahemikus 1-50: 
         <input type="number" name="kasutajaArv"><br />
+        <input type="hidden" name="katseteArv" value="'.$katseteArv.'">
+        <input type="hidden" name="serveriArv" value="'.$serveriArv.'">
         <input type="submit" value="Kontrolli">
     </form>
 ';
 // kontrollime, kas arv on edastatud
 if(!empty($_POST['kasutajaArv'])){
     // määrame arvu
-    $serveriArv = 50;
     $kasutajaArv = $_POST['kasutajaArv'];
     if($kasutajaArv > $serveriArv){
         echo 'Pakutud väärtus on suurem<br />';
@@ -27,6 +34,7 @@ if(!empty($_POST['kasutajaArv'])){
         if($kasutajaArv == $serveriArv){
             echo 'Arvasid ära!<br />';
             echo 'Arv on '.$serveriArv.'<br />';
+            echo 'Arvu ära aramiseks läks '.$katseteArv.' korda<br />';
             exit;
         }
         echo 'Oled juba hästi lähedal<br />';
